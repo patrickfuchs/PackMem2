063ca06287ad5c7c8cf99e04e7a9fa0496be54a6
MayaZygadlo <maya.zygadlo@yahoo.fr>
2024-09-03 15:26:14 +0200

    Fuse the old Concatenate_PackMem with the protein one and add real argument to add or not protein analysis

--- src/Concatenate_PackMem_prot.sh
+++ src/Concatenate_PackMem_prot.sh
@@ -1,15 +1,50 @@
 #!/bin/bash
 
-# Script to concatenate the PackMem analysis on multiple PDB files.
+usage(){
+cat <<EOF
 
-# set prefix of the pdb files
-prefix=$1
+usage : $0 -n prefix -b first_frame -e last_frame options
 
-# set the first frame
-first_frame=$2
+This script concatenates the PackMem analysis on multiple frame.
 
-# set the last frame
-last_frame=$3
+Options to specify input files:
+    -n prefix of the files
+    -b The first frame of the PackMem analysis
+    -e The last frame of the PackMem analysis
+
+OPTIONS:
+    -p If there is a protein (Dafult : false)
+
+EOF
+}
+
+prot=false
+
+while getopts "hn:b:e:p" OPTION
+do
+    case $OPTION in
+        h)
+            usage
+            exit 1
+            ;;
+        n)
+            prefix=$OPTARG
+            ;;
+        b)
+            first_frame=$OPTARG
+            ;;
+        e)
+            last_frame=$OPTARG
+            ;;
+        p)
+            prot=true
+            ;;
+        ?)
+            usage
+            exit
+            ;;
+    esac
+done
 
 
 # The following Total_*.txt files will contain the statistics for each type of
@@ -28,6 +63,25 @@ last_frame=$3
 for pdbnum in $(seq ${first_frame} ${last_frame})
 do
     # Accumulate packing defects of the current frame in Total_*.txt files per leaflet
+     cat  ${prefix}${pdbnum}_Up_Deep_result.txt >> Total_Up_${prefix}_deep.txt
+     cat  ${prefix}${pdbnum}_Lo_Deep_result.txt >> Total_Lo_${prefix}_deep.txt
+     cat  ${prefix}${pdbnum}_Up_Shallow_result.txt >> Total_Up_${prefix}_shallow.txt
+     cat  ${prefix}${pdbnum}_Lo_Shallow_result.txt >> Total_Lo_${prefix}_shallow.txt
+     cat  ${prefix}${pdbnum}_Up_All_result.txt >> Total_Up_${prefix}_all.txt
+     cat  ${prefix}${pdbnum}_Lo_All_result.txt >> Total_Lo_${prefix}_all.txt
+
+    # we no longer need the defects of the current frame
+    # rm -f ${prefix}${pdbnum}_Up_Deep_result.txt
+    # rm -f ${prefix}${pdbnum}_Lo_Deep_result.txt
+    # rm -f ${prefix}${pdbnum}_Up_Shallow_result.txt
+    # rm -f ${prefix}${pdbnum}_Lo_Shallow_result.txt
+    # rm -f ${prefix}${pdbnum}_Up_All_result.txt
+    # rm -f ${prefix}${pdbnum}_Lo_All_result.txt
+done
+
+if [ ${prot} = true ]
+then
+    # Accumulate packing defects of the current frame in Total_*.txt files per leaflet
      cat  ${prefix}${pdbnum}_Up_All_prot.txt >> Total_Up_${prefix}_all_prot.txt
      cat  ${prefix}${pdbnum}_Up_Deep_prot.txt >> Total_Up_${prefix}_deep_prot.txt
      cat  ${prefix}${pdbnum}_Up_Shallow_prot.txt >> Total_Up_${prefix}_shallow_prot.txt
@@ -36,4 +90,27 @@ do
      rm -f ${prefix}${pdbnum}_Up_All_prot.txt
      rm -f ${prefix}${pdbnum}_Up_Deep_prot.txt
      rm -f ${prefix}${pdbnum}_Up_Shallow_prot.txt
-done
+fi
+
+# Concatenate the packing defects statistics of the lower and upper leaflets
+cat Total_Up_${prefix}_deep.txt Total_Lo_${prefix}_deep.txt > Total_${prefix}_deep.txt
+cat Total_Up_${prefix}_shallow.txt Total_Lo_${prefix}_shallow.txt > Total_${prefix}_shallow.txt
+cat Total_Up_${prefix}_all.txt Total_Lo_${prefix}_all.txt > Total_${prefix}_all.txt
+
+# Clean txt file for python script
+egrep -v "#" Total_${prefix}_deep.txt > Total_${prefix}_deep_clean.txt
+egrep -v "#" Total_${prefix}_shallow.txt > Total_${prefix}_shallow_clean.txt
+egrep -v "#" Total_${prefix}_all.txt > Total_${prefix}_all_clean.txt
+
+# Clean leaflet txt file for python script
+egrep -v "#" Total_Up_${prefix}_deep.txt > Total_Up_${prefix}_deep_clean.txt
+egrep -v "#" Total_Up_${prefix}_shallow.txt > Total_Up_${prefix}_shallow_clean.txt
+egrep -v "#" Total_Up_${prefix}_all.txt > Total_Up_${prefix}_all_clean.txt
+egrep -v "#" Total_Lo_${prefix}_deep.txt > Total_Lo_${prefix}_deep_clean.txt
+egrep -v "#" Total_Lo_${prefix}_shallow.txt > Total_Lo_${prefix}_shallow_clean.txt
+egrep -v "#" Total_Lo_${prefix}_all.txt > Total_Lo_${prefix}_all_clean.txt
+
+# Finally remove the the non clean .txt files
+rm -f Total_*${prefix}_deep.txt
+rm -f Total_*${prefix}_shallow.txt
+rm -f Total_*${prefix}_all.txt
