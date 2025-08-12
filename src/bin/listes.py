85a7a83a8e60e9f7d86398917cd221c6d7512acd
CNOV0 <mayazygadlo@DESKTOP-TARTU7S.localdomain>
2025-08-12 10:03:59 +0200

    Put create_arrayZ in listes.py

--- src/bin/listes.py
+++ src/bin/listes.py
@@ -19,6 +19,48 @@ def create_array(v1, v2, step):
     """
     return np.arange(v1, v2, step)
 
+def create_arrayZ(residues, list_resids, atom_mb, dist_suppl_Z, z_extr, up=True):
+    """
+    Create the listZ array for upper and lower leaflet
+    ---------------------------------------------------------------------------
+    INPUT:
+    residues : MDAnalysis ResidueGroup
+        Contains the names of all the residues selected
+    list_resids : numpy array
+        Contains all the residue numbers selected
+    atom_mb : str
+        Name of the reference atom for the lipids (C2)
+    dist_suppl_Z : float
+        Supplementary distance from the z coord
+    z_extr : float
+        Either maximum or minimum value of z coord
+    up : boolean
+        If we are on the upper leaflet
+    ---------------------------------------------------------------------------
+    OUTPUT:
+    numpy array
+        Contains floats ranging from z_coord to zmin
+        or from zmax to z_coord by 1.0 steps
+    """
+    leaflet_listZ = {}
+    for resid in list_resids:
+        # Get the residue at a given residue number (resid)
+        residue = residues[residues.resids == resid][0]
+        # Get the atom given in the parameter file
+        atom = residue.atoms[residue.atoms.names == atom_mb][0]
+        # Get the z of this atom
+        z_coord = atom.position[2]
+        if up:
+            tmp = l.create_array(round(z_coord - dist_suppl_Z, 2),
+                                        round(z_extr +1.0, 2), m.SIZE)
+        else:
+            tmp = l.create_array(round(z_coord + dist_suppl_Z, 2),
+                                            round(z_extr - 1.0, 2), -m.SIZE)
+        # Reverse it
+        tmp = np.flip(tmp)
+        leaflet_listZ[resid] = tmp
+    return leaflet_listZ
+
 #from list[list] determine max length
 def determine_lenMax(listeTot):
     return max([len(data) for data in listeTot])
