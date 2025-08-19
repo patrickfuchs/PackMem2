8793a68baa256ee7ee53dea01ef76615ebe80706
CNOV0 <mayazygadlo@DESKTOP-TARTU7S.localdomain>
2025-08-19 11:28:03 +0200

    Add test function to min_max_mean

--- tests/test_array.py
+++ tests/test_array.py
@@ -16,3 +16,13 @@ def test_get_glyc_lipids():
     tested_output = l.get_glyc_lipids(lipids, glycerol)
     wanted_output = 'C2'
     assert tested_output == wanted_output
+
+def test_min_max_mean():
+    data = [1, 0.4, 5.6, 0.89]
+    out_min, out_max, out_mean = l.min_max_mean(data)
+    wanted_min = 0.4
+    wanted_max = 5.6
+    wanted_mean = 1.9725
+    assert out_min == wanted_min
+    assert out_max == wanted_max
+    assert pytest.approx(out_mean, 0.001) == wanted_mean
