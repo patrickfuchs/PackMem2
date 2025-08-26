a5d7b9ee6da5d6175d2188772eab245f079905a1
CNOV0 <mayazygadlo@DESKTOP-TARTU7S.localdomain>
2025-08-26 17:42:30 +0200

    Delete useless comments

--- tests/test_pdb.py
+++ tests/test_pdb.py
@@ -12,15 +12,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 's
 from bin import pdb as pdb
 
 def test_write_a_pdb_line():
-    # Create fake file
-    #fake_file = io.StringIO()
-    # Write
     content = pdb.write_a_pdb_line(1, 'EDG', 1, [-8.0, -9.0, 7.310], -1.0)
-
-    # Read the fake file
-    #fake_file.seek(0)  # Seek the start of the file
-    #content = fake_file.read()
-
     wanted_line = f"ATOM      1   H1 EDG     1      -8.000  -9.000   7.310  1.00 -1.00\n"
     assert content == wanted_line
 
