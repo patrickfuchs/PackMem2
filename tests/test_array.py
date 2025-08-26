be17f1c216ef10c8212795e71db4c60b96b055f3
CNOV0 <mayazygadlo@DESKTOP-TARTU7S.localdomain>
2025-08-26 10:14:55 +0200

    Finish and add test function for the listes module

--- tests/test_array.py
+++ tests/test_array.py
@@ -1,5 +1,6 @@
 import pytest
 import numpy as np
+import MDAnalysis as mda
 
 import sys
 import os
@@ -33,10 +34,28 @@ def test_create_array():
     np.testing.assert_array_equal(tested_output, wanted_output)
 
 def test_create_arrayZ():
-    residues = [] # pb ici car objet MDAnalysis
+    n_atoms = 2
+    u = mda.Universe.empty(n_atoms=n_atoms,
+                           n_residues=n_atoms,
+                           atom_resindex=list(range(n_atoms)),
+                           trajectory=True)
+
+    # Add atom names
+    atom_name = "C2"
+    u.add_TopologyAttr("name", [atom_name] * n_atoms)
+
+    # Add residue IDs
     list_resids = [1, 2]
-    atom = 'C2'
-    tested_output = l.create_arrayZ(residues, list_resids, atom, 1.0, 13.0)
-    # supposons z_coord = 7 puis 10
+    u.add_TopologyAttr("resid", list_resids)
+
+    # Add coordinates
+    coords = np.zeros((n_atoms, 3))
+    # Specify the z values
+    coords[:, 2] = [7.0, 10.0]
+    u.atoms.positions = coords
+
+    tested_output = l.create_arrayZ(u.residues, list_resids, atom_name, 1.0, 13.0, up=True)
     wanted_output = {1: [13., 12., 11., 10., 9., 8., 7., 6.], 2: [13., 12., 11., 10., 9.]}
-    assert tested_output == wanted_output
+    # Compare each element
+    for resid in wanted_output:
+        assert np.allclose(tested_output[resid], wanted_output[resid])
